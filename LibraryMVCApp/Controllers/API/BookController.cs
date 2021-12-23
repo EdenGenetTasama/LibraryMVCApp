43758ed574df22c2475e9941d7e870d6f8a52660
EdenGenetTasama <edentasama19@gmail.com>
2021-12-23 10:19:55 +0200

    add linq

--- LibraryMVCApp/Controllers/API/BookController.cs
+++ LibraryMVCApp/Controllers/API/BookController.cs
@@ -22,14 +22,14 @@ namespace LibraryMVCApp.Controllers.API
             Random random = new Random();
             Book[] bookArray = new Book[]
             {
-                new Book("BookOne", "WritterOne", 2010, random.Next(200, 500),1),
-                new Book("BookTwo", "WritterTwo", 2000, random.Next(200, 500),2),
-                new Book("BookThree", "WritterThree", 2003, random.Next(200, 500),3),
-                new Book("BookFour", "WritterFour", 2004, random.Next(200, 500),4),
-                new Book("BookFive", "WritterFive", 2005, random.Next(200, 500),5),
-                new Book("BookSix", "WritterSix", 2006, random.Next(200, 500),6),
-                new Book("BookSeven", "WritterSeven", 2007, random.Next(200, 500),7),
-                new Book("BookEigth", "WritterEigth", 2008, random.Next(200, 500),8)
+                new Book("BookOne", "WritterOne", 2010, random.Next(100, 500),1),
+                new Book("BookTwo", "WritterTwo", 2000, random.Next(100, 500),2),
+                new Book("BookThree", "WritterThree", 2003, random.Next(100, 500),3),
+                new Book("BookFour", "WritterFour", 2004, random.Next(100, 500),4),
+                new Book("BookFive", "WritterFive", 2005, random.Next(100, 500),5),
+                new Book("BookSix", "WritterSix", 2006, random.Next(100, 500),6),
+                new Book("BookSeven", "WritterSeven", 2007, random.Next(100, 500),7),
+                new Book("BookEigth", "WritterEigth", 2008, random.Next(100, 500),8)
             };
 
             this.bookList.AddRange(bookArray);
@@ -54,15 +54,20 @@ namespace LibraryMVCApp.Controllers.API
         [HttpGet]
         public ActionResult DetailsByName(string nameOfBook)
         {
-            ListOfBooks();
-            foreach (Book book in bookList)
+            try
             {
-                if (nameOfBook == book.Name)
-                {
-                    return Json(book, JsonRequestBehavior.AllowGet);
-                }
+                List<Book> byName = (from book in bookList
+                                     where nameOfBook == book.Name
+                                     //orderby book.YearOfOut
+                                     select book).ToList();
+
+                return Json(bookList, JsonRequestBehavior.AllowGet);
             }
-            return Json("NOT FOUND", JsonRequestBehavior.AllowGet);
+            catch
+            {
+
+                return Json("NOT FOUND", JsonRequestBehavior.AllowGet);
+            }
 
         }
 
@@ -81,31 +86,33 @@ namespace LibraryMVCApp.Controllers.API
 
         // POST: Person/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Book obj)
         {
             try
             {
                 //ListOfBooks();
                 //Book bookCreate = new Book("BookNine", "WritterNine", 2009, 900, 9);
-                //bookList.Add(bookCreate);
+                bookList.Add(obj);
                 // TODO: Add insert logic here
 
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return Json("NOT FOUND", JsonRequestBehavior.AllowGet);
             }
         }
 
 
         [HttpPut]
         // PUT: Person/Edit/5
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Book obj)
         {
             try
             {
-                // TODO: Add delete logic here
+                var update = from book in bookList
+                             where id == book.id
+                             select book;
 
                 return RedirectToAction("Index");
             }
@@ -119,15 +126,16 @@ namespace LibraryMVCApp.Controllers.API
         [HttpDelete]
         public ActionResult Delete(int id)
         {
-            foreach (Book book in bookList)
+            try
             {
-                if (id == book.id)
-                {
-                    bookList.Remove(book);
-                    return RedirectToAction("index");
-                }
+                //Book deletePerson = bookList.Find(item => item.id == id);
+                bookList.Remove(bookList.Find(item => item.id == id));
+                return Json("Removed", JsonRequestBehavior.AllowGet);
             }
-            return Json("NOT FOUND", JsonRequestBehavior.AllowGet);
+            catch
+            {
+                return Json("NOT FOUND", JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
